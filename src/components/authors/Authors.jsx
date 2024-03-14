6051c72099b8efffd1bb22be021392775e55128f
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-03-14 11:24:14 +0330

    Authors component done

--- src/components/authors/Authors.jsx
+++ src/components/authors/Authors.jsx
@@ -3,6 +3,8 @@ import React from "react";
 // graphql
 import { useQuery } from "@apollo/client";
 import { GET_AUTHORS_INFO } from "../../graphql/queries";
+// MUI
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
 
 const Authors = () => {
   const { data, loading } = useQuery(GET_AUTHORS_INFO);
@@ -10,9 +12,37 @@ const Authors = () => {
   console.log(data);
   return (
     <div>
-      {data.authors.map((author) => {
-        return <h1>{author.name}</h1>;
-      })}
+      <Grid
+        container
+        sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}
+      >
+        {data.authors.map((author,index) => {
+          return (
+            <React.Fragment key={author.id}>
+              <Grid item xs={12} padding={2}>
+                <a
+                  href={`authors/${author.slug}`}
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    textDecoration: "none",
+                  }}
+                >
+                  <Avatar src={author.avatar.url} sx={{ marginRight: 2 }} />
+                  <Typography component="p" variant="p" color="text.secondary">
+                    {author.name}
+                  </Typography>
+                </a>
+              </Grid>
+              {index !== data.authors.length -1 && 
+              <Grid item xs={12}>
+              <Divider variant="middle" />
+              </Grid>
+              }
+            </React.Fragment>
+          );
+        })}
+      </Grid>
     </div>
   );
 };
